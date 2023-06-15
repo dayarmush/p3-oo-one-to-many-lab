d112b1ada04b41a17ed09cbd62e62a0f78bf2767
Dovid Yarmush <yarmushda@gmail.com>
2023-06-15 16:39:23 -0400

    coplete one to many object relations lB

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,39 @@
 class Pet:
-    pass
+
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner = None):
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+        Pet.all.append(self)
+
+    @property
+    def pet_type(self):
+        return self._pet_type
+        
+    @pet_type.setter
+    def pet_type(self, new_pet):
+        if new_pet in Pet.PET_TYPES:
+            self._pet_type = new_pet
+        else:
+            raise Exception('invalid pet type')
+        
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+    
+
+    def pets(self):
+        return Pet.all
+    
+    def add_pet(self, pet):
+        if isinstance(pet, Pet):
+            pet.owner = self
+        else:
+            raise Exception('Invalid pet type')
+        
+    def get_sorted_pets(self):
+        return sorted(Pet.all, key=lambda pet: pet.name)
